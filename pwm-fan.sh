3481c71704bc14052881e2a01a59f0eb04350f2d
CFA Gomes <cf365@cornell.edu>
2020-10-10 14:48:12 -0300

    still testing

--- pwm-fan.sh
+++ pwm-fan.sh
@@ -45,6 +45,7 @@ cache () {
 }
 
 cleanup () {
+	# TODO: stop and free up used pins
 	# remove cache files
 	if [[ -d "$CACHE_ROOT" ]]; then
 		rm -rf "$CACHE_ROOT"
@@ -52,14 +53,6 @@ cleanup () {
 }
 
 unexport_pwmchip1_channel () {
-	# set channel for the pwmchip1
-	local CHANNEL="$1"
-	if [[ -z "$CHANNEL" ]]; then
-		# if no arg provided, assume ch 0
-		local CHANNEL='pwm0'
-	fi
-	PWMCHIP1_FOLDER='/sys/class/pwm/pwmchip1/'
-	CHANNEL_FOLDER="$PWMCHIP1_FOLDER""$CHANNEL"'/'
 	if [[ -d "$CHANNEL_FOLDER" ]]; then
 	    local UNEXPORT='/sys/class/pwm/pwmchip1/unexport'
 	    cache 'unexport'
@@ -68,20 +61,10 @@ unexport_pwmchip1_channel () {
 	    # TODO: Handle unexport errors
 	elif [[ ! -d "$CHANNEL_FOLDER" ]]; then
 		echo '[pwm-fan] It seems channel '$CHANNEL' has already been freed up or is not controlled by pwmchip1.'
-		echo '[pwm-fan] For reference, pwmchip1 supports '$(cat $PWMCHIP1_FOLDER'npwm')' channels'
 	fi
 }
 
-# accept channel as argument
 export_pwmchip1_channel () {
-	# set channel for the pwmchip1
-	CHANNEL="$1"
-	if [[ -z "$CHANNEL" ]]; then
-		# if no arg provided, assume ch 0
-		CHANNEL='pwm0'
-	fi
-	PWMCHIP1_FOLDER='/sys/class/pwm/pwmchip1/'
-	CHANNEL_FOLDER="$PWMCHIP1_FOLDER""$CHANNEL"'/'
 	if [[ ! -d "$CHANNEL_FOLDER" ]]; then
 	    local EXPORT='/sys/class/pwm/pwmchip1/export'
 	    cache 'export'
@@ -114,26 +97,115 @@ export_pwmchip1_channel () {
 	fi
 }
 
-fan_startup () {
-	if [[ -z "$CHANNEL" || -z "$CHANNEL_FOLDER" ]]; then
-		echo '[pwm-fan] Something is wrong. The channel has not been set before.'
-		end 'Trying to start the fan without a channel.' 1
+interrupt () {
+	echo '!! ATTENTION !!'
+	end 'Received a signal to stop the script.' 1
+}
+
+pwmchip1 () {
+	PWMCHIP1_FOLDER='/sys/class/pwm/pwmchip1/'
+	if [[ ! -d "$PWMCHIP1_FOLDER" ]]; then
+		echo '[pwm-fan] The sysfs interface for the pwmchip1 is not accessible.'
+		end 'Cannot access pwmchip1 sysfs interface.' 1
 	fi
-	while [[ ! $ERR_CONFIG && -d "$CHANNEL_FOLDER" ]]; do
-		local READ_ENABLE=$(cat $CHANNEL_FOLDER'enable')
-		local READ_PERIOD=$(cat $CHANNEL_FOLDER'period')
-		local READ_DUTY_CYCLE=$(cat $CHANNEL_FOLDER'duty_cycle')
-		local READ_POLARITY=$(cat $CHANNEL_FOLDER'polarity')
+	echo '[pwm-fan] Working with the sysfs interface for the pwmchip1.'
+	echo '[pwm-fan] For reference, your pwmchip1 supports '$(cat $PWMCHIP1_FOLDER'npwm')' channels.'
+	# set channel for the pwmchip1
+	CHANNEL="$1"
+	if [[ -z "$CHANNEL" ]]; then
+		# if no arg provided, assume ch 0
+		CHANNEL='pwm0'
+	fi
+	CHANNEL_FOLDER="$PWMCHIP1_FOLDER""$CHANNEL"'/'
+}
+
+set_default () {
+	cache 'set_default_duty_cycle'
+	SET_DUTY_CYCLE=$(echo 0 2> $CACHE > $CHANNEL_FOLDER'duty_cycle')
+	if [[ ! -z $(cat $CACHE) ]]; then
+		echo 'bad set_duty_cycle'
+		SET_PERIOD=$(echo 1000 > $CHANNEL_FOLDER'period')
+		SET_DUTY_CYCLE=$(echo 0 > $CHANNEL_FOLDER'duty_cycle')
+	fi
+	cache 'set_default_period'
+	SET_PERIOD=$(echo $PERIOD 2> $CACHE > $CHANNEL_FOLDER'period')
+	if [[ ! -z $(cat $CACHE) ]]; then
+		echo '[pwm-fan] The period provided ('$PERIOD') is not acceptable.'
+		echo '[pwm-fan] Trying to lower it by 100ns decrements. This may take a while.'
+		local PERIOD_=$PERIOD
+		local rate=100
+		local decrement=$rate
+		until [[ $PERIOD_ -le 1 ]]; do
+			local PERIOD_=$(($PERIOD-$decrement))
+			if [[ $PERIOD_ -lt 1 ]]; then
+				local PERIOD_=1
+			fi
+			echo $PERIOD_
+			> $CACHE
+			SET_PERIOD=$(echo $PERIOD_ 2> $CACHE > $CHANNEL_FOLDER'period')
+			if [[ -z $CACHE ]]; then
+				break
+			fi
+			local decrement=$((decrement+$rate))
+		done
+		PERIOD=$PERIOD_
+		if [[ $PERIOD -le 50 ]]; then
+			end 'Unable to set an appropriate value for the period' 1
+		fi
+		echo '[pwm-fan] Current period is now '$PERIOD
+	fi
+	SET_POLARITY=$(echo 'normal' > $CHANNEL_FOLDER'polarity')
+	READ_POLARITY=$(cat $CHANNEL_FOLDER'polarity')
+	READ_PERIOD=$(cat $CHANNEL_FOLDER'period')
+	READ_DUTY_CYCLE=$(cat $CHANNEL_FOLDER'duty_cycle')
+	echo '[pwm-fan] Default polarity was set to: '$READ_POLARITY
+	echo '[pwm-fan] Default period was set to: '$READ_PERIOD' ns'
+	echo '[pwm-fan] Default duty cycle was set to: '$READ_DUTY_CYCLE' ns of active time'
+}
+
+# TODO: need to finish this function
+fan_startup () {
+	PERIOD="$1"
+	if [[ -z $PERIOD ]]; then
+		# default period is 25kHz / 40000 nanoseconds
+		PERIOD=40000
+	elif [[ ! $PERIOD =~ ^[0-9]*$ ]]; then
+		echo '[pwm-fan] The period must be an integer greater than 0.'
+		end 'Period is not integer' 1
+	fi
+	while [[ ! $CONFIG_READY && -d "$CHANNEL_FOLDER" ]]; do
+		READ_ENABLE=$(cat $CHANNEL_FOLDER'enable')
+		if [[ $READ_ENABLE -eq 0 ]]; then
+			# fan is not enabled
+			set_default
+			CONFIG_READY=1
+		elif [[ $READ_ENABLE -eq 0 ]]; then
+			# fan is enabled
+			echo '[pwm-fan] The fan is already enabled. Will disable it.'
+			SET_ENABLE=$(echo 0 > $CHANNEL_FOLDER'enable')
+			sleep 1
+			set_default
+			CONFIG_READY=1
+		else
+			echo '[pwm-fan] Unable to read the fan enable status.'
+			end 'Bad fan status' 1
+		fi
 	done
 }
 
+# takes channel (pwmN) and period (integer in ns) as arg
 config () {
-	export_pwmchip1_channel pwm0
-	fan_startup
+	pwmchip1 "$1"
+	export_pwmchip1_channel
+	fan_startup "$2"
 }
 
-config 
-
+# run pwm-fan
+start
+trap 'interrupt' SIGINT SIGHUP SIGTERM SIGKILL
+# user may provide custom period as first arg
+config pwm0 $1
+end 'Finished without any errors' 0
 
 
 # Set default period (40000ns = 25kHz)
